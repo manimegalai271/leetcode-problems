594666b7c70f7a83bbc449caabc52806961262e9
Manimegalai k <manimegalaik2710@gmail.com>
2025-06-24 21:20:53 +0530

    Time: 0 ms (100%), Space: 42.1 MB (32.9%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Solution {
+    public String sortColors(int[] nums) {
+        int[] count=new int[3];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            {
+                count[0]++;
+            }
+            else if(nums[i]==1)
+            {
+                count[1]++;
+            }
+            else 
+            {
+                count[2]++;
+            }
+        }
+            int o=0;
+            for(int i=0;i<count.length;i++)
+            {
+            for(int j=0;j<count[i];j++)
+            {
+                nums[o]=i;
+                o++;
+            }
+            }
+            return Arrays.toString(nums);
+    }
+}
