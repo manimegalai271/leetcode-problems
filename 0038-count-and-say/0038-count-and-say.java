2483db7a75ca69e85f2b289a2690d5fb5e1baff6
Manimegalai k <manimegalaik2710@gmail.com>
2025-07-07 14:45:37 +0530

    Time: 27 ms (5.18%), Space: 45.1 MB (15.95%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,25 +1,26 @@
 class Solution {
     public String countAndSay(int n) {        
-        String curr = "1";
-        for(int k = 1;k<n;k++)
-        {            
-            String next = "";
-            int i =0;
-            while(i < curr.length())
+        String s="1";
+        for(int i=1;i<n;i++)
+        {
+            int j=0;
+            String next="";
+            while(j<s.length())
             {
-                int currentcharcount = 1;
-                while(i < curr.length()- 1 && (curr.charAt(i) == curr.charAt(i + 1)))
-                {
-                    currentcharcount +=1;
-                    i = i+1;
-                }
-                next += Integer.toString(currentcharcount) + curr.charAt(i);
-                i = i + 1;                               
-            }
-            curr = next;
+                int count=1;
+                while(j<s.length()-1 && s.charAt(j)==s.charAt(j+1))
+                {   
 
+                    count++;
+                    j++;
+                }
+                next += Integer.toString(count) + s.charAt(j);
+
+                j++;
+            }
+            s=next;
         }
-        return curr;
+        return s;
         
     }
 }
