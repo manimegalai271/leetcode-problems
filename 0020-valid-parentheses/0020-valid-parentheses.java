b3cec48406082bd5e385185bd7827e89644923b9
Manimegalai k <manimegalaik2710@gmail.com>
2025-08-22 20:25:39 +0530

    Time: 2 ms (96.63%), Space: 41.8 MB (47.71%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,60 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-		Stack<Character> ch=new Stack<Character>();
-		for(int i=0;i<s.length();i++)
-		{
-		    if(s.charAt(i)=='[' || s.charAt(i)=='{' || s.charAt(i)=='(')
-		    {
-		        ch.push(s.charAt(i));
-		    }
-		    else 
-		    {
-		        if(ch.isEmpty())
-		        {
-		            return false;
-		        }
-		        else if(s.charAt(i)==']')
-		        {
-		            if(ch.peek()=='[')
-		            {
-		                ch.pop();
-		            }
-		            else 
-		            {
-		                return false;
-		            }
-		        }
-		        else if(s.charAt(i)==')')
-		        {
-		            if(ch.peek()=='(')
-		            {
-		                ch.pop();
-		            }
-		            else 
-		            {
-		                return false;
-		            }
-		        }
-		        else if(s.charAt(i)=='}')
-		        {
-		            if(ch.peek()=='{')
-		            {
-		                ch.pop();
-		            }
-		            else 
-		            {
-		                return false;
-		            }
-		        }
-		    }
-		}
-		if(ch.isEmpty())
-		{
-		    return true;
-		}
-		else 
-		{
-		    return false;
-		}
+        Map<Character,Character> hm=new HashMap(){{
+            put('}','{');
+            put(']','[');
+            put(')','(');
+        }};
+        Stack<Character> p=new Stack();
+        char ch[] = s.toCharArray();
+        for(char c:ch)
+        {
+           if(c == '(' || c=='{' || c=='[')
+           {
+            p.push(c);
+           }
+           else if(p.isEmpty() || p.pop()!=hm.get(c))
+           {
+            return false;
+           }
+        }
+        return p.isEmpty();
+
+        
     }
 }
