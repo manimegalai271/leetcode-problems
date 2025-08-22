ff5e404b15ef017bb1e54f85a265e123833c89ca
Manimegalai k <manimegalaik2710@gmail.com>
2025-08-22 21:14:57 +0530

    Time: 4 ms (34.94%), Space: 44.5 MB (26.11%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans=new int[nums1.length];
-        for(int i = 0;i < nums1.length;i++)
+        Map<Integer,Integer> map=new HashMap<>();
+        Deque<Integer> s=new ArrayDeque();
+        int[] res=new int[nums1.length];
+        for(int ele:nums2)
         {
-            ans[i] = -1;
-            Boolean flag = false;
-            for(int j = 0;j < nums2.length;j++)
+            while(!s.isEmpty() && s.peek()<ele)
             {
-                if(nums1[i] == nums2[j] )
-                {
-                    flag = true;
-                }
-                if(flag && nums1[i] < nums2[j])
-                {
-                    ans[i] = nums2[j];
-                    break;
-                }
+                map.put(s.pop(),ele);
             }
+            s.push(ele);
+
         }
-        return ans;
+        for(int i=0;i<nums1.length;i++)
+        {
+            res[i] = map.containsKey(nums1[i])?map.get(nums1[i]):-1;
+        }
+        return res;
+
     }
 }
