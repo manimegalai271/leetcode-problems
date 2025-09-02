4989faa8a915e22d497292e757a28c3529dc66c0
Manimegalai k <manimegalaik2710@gmail.com>
2025-09-02 11:37:42 +0530

    Time: 0 ms (100%), Space: 40.6 MB (37.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,8 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0 || n == 1)
-        {
-            return n;
-        }
-        return fib(n-1) + fib(n-2);
+        double sqrt5 = Math.sqrt(5);
+        double phi = (1 + sqrt5) / 2;       // golden ratio
+        double psi = (1 - sqrt5) / 2;       // conjugate of phi
+        return (int) Math.round((Math.pow(phi, n) - Math.pow(psi, n)) / sqrt5);
     }
 }
