c1a2933c721692580b48f13ff63f14390cb604f2
Manimegalai k <manimegalaik2710@gmail.com>
2025-09-12 12:05:51 +0530

    Time: 6 ms (9.26%), Space: 42.7 MB (5.42%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,51 +1,58 @@
 class Solution {
-    boolean canPlace(int row, int col, List<String> curr) {
-        for (int i = row - 1; i >= 0; i--)
-            if (curr.get(i).charAt(col) == 'Q')
-                return false;
-
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
-            if (curr.get(i).charAt(j) == 'Q')
-                return false;
-
-        for (int i = row - 1, j = col + 1; i >= 0 && j < curr.size(); i--, j++)
-            if (curr.get(i).charAt(j) == 'Q')
-                return false;
-
+    public Boolean canreplace(int row,int col,List<String> curr)
+    {
+        for(int i = row-1;i>=0;i--)
+        {
+            if(curr.get(i).charAt(col) == 'Q')
+            {
+               return false;
+            }
+        }
+        for(int i = row-1,j = col-1;i>=0 && j>=0;i--,j--)
+        {
+            if(curr.get(i).charAt(j) == 'Q')
+            {
+               return false;
+            }
+        }
+        for(int i = row-1,j = col+1;i>=0 && j<curr.size();i--,j++)
+        {
+            if(curr.get(i).charAt(j) == 'Q')
+            {
+               return false;
+            }
+        }
         return true;
     }
-
-    void backtrack(int row, List<String> curr, List<List<String>> res) {
-        if (row == curr.size()) {
-            res.add(new ArrayList<>(curr));
+    public void backtrack(int row,List<String> curr,List<List<String>> ans,int n)
+    {
+        if(row == curr.size())
+        {
+            ans.add(new ArrayList(curr));
             return;
         }
+        for(int col = 0;col < curr.size();col++)
+        {
+            if(canreplace(row,col,curr))
+            {
+                StringBuilder sb = new StringBuilder(curr.get(row));
+                sb.setCharAt(col,'Q');
+                curr.set(row,sb.toString());
+                backtrack(row+1,curr,ans,n);
+                sb.setCharAt(col,'.');
+                curr.set(row,sb.toString());
 
-        int n = curr.size();
-        for (int col = 0; col < n; col++) {
-            if (canPlace(row, col, curr)) {
-                char[] rowArr = curr.get(row).toCharArray();
-                rowArr[col] = 'Q';
-                curr.set(row, new String(rowArr));
-
-                backtrack(row + 1, curr, res);
-
-                rowArr[col] = '.';
-                curr.set(row, new String(rowArr));
             }
         }
     }
     public int totalNQueens(int n) {
-        List<List<String>> res = new ArrayList<>();
+        List<List<String>> ans = new ArrayList<>();
         List<String> curr = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            char[] row = new char[n];
-            Arrays.fill(row, '.');
-            curr.add(new String(row));
+        for(int i = 0;i < n;i++)
+        {
+            curr.add(".".repeat(n));
         }
-
-        backtrack(0, curr, res);
-        return res.size();
+        backtrack(0,curr,ans,n);
+        return ans.size();
     }
 }
