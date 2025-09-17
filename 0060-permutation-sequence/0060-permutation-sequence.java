184ecd633848d4f4a80642376e296fd2fa69baf5
Manimegalai k <manimegalaik2710@gmail.com>
2025-09-17 19:48:27 +0530

    Time: 1623 ms (5.03%), Space: 134.3 MB (5%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        int[] nums = new int[n];
+        for(int i = 1;i<=n;i++)
+        {
+            nums[i-1] = i;
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(ans, new ArrayList<>(), nums, new boolean[nums.length]);
+        int count = 0;
+        String s = "";
+        for(List<Integer> a:ans)
+        {
+            if(count == k-1)
+            {
+                for(int str:a)
+                {
+                    s=s+str;
+                }
+            }
+            count++;
+        }
+        return s;
+
+    }
+    public static void backtrack(List<List<Integer>> ans, List<Integer> curr, int[] nums, boolean[] used) {
+        if (curr.size() == nums.length) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                curr.add(nums[i]);
+
+                backtrack(ans, curr, nums, used);
+
+                curr.remove(curr.size() - 1);
+                used[i] = false;
+            }
+        }
+    }
+}
