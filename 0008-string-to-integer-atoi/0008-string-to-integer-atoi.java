f86182b13c3ae3dd57be42983a1e4c86251c6acb
Manimegalai k <manimegalaik2710@gmail.com>
2025-09-23 13:11:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,36 +1,35 @@
 class Solution {
     public int myAtoi(String s) {
-        if (s == null) return 0;
-
+        if(s == null)return 0;
         s = s.trim();
-        if (s.length() == 0) return 0;
-
-        int sign = 1;
         int i = 0;
-        if (s.charAt(0) == '-') {
+        if(s.length() == 0)return 0;
+        int sign = +1;
+        if(s.charAt(0) == '-')
+        {
             sign = -1;
             i++;
-        } else if (s.charAt(0) == '+') {
+        }
+        else if(s.charAt(0) == '+')
+        {
             i++;
         }
-
         long ans = 0;
-        int max = Integer.MAX_VALUE;
-        int min = Integer.MIN_VALUE;
-
-        while (i < s.length()) {
+        int max = Integer.MAX_VALUE,min = Integer.MIN_VALUE;
+        while(i < s.length())
+        {   
             char ch = s.charAt(i);
-            if (!Character.isDigit(ch)) break;
-
-            ans = ans * 10 + (ch - '0');
-
-            // Overflow checks
-            if (sign == 1 && ans > max) return max;
-            if (sign == -1 && -ans < min) return min;
-
+            if(!Character.isDigit(ch))
+            {
+                break;
+            }
+            ans = ans*10 + s.charAt(i)-'0';
+            if(sign ==1 && ans >max)return max;
+            else if(sign == -1 && ans<min) return min;
+            
             i++;
         }
+        return (int) (sign*ans);
 
-        return (int)(sign * ans);
     }
 }
