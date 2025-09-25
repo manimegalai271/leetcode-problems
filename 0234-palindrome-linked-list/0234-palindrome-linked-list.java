4c819e0d08b74acab60014f41d753b37e8ef5bb8
Manimegalai k <manimegalaik2710@gmail.com>
2025-09-25 15:05:43 +0530

    Time: 1313 ms (5.6%), Space: 58.3 MB (81.26%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -34,3 +34,86 @@ class Solution {
         return true;
     }
 }
+/* 
+class ListNode 
+{
+    int val;
+    ListNode next;
+    ListNode(int val)
+    {
+        this.val = val;
+    }
+    
+}
+class LinkedList 
+{   
+    ListNode head;
+    ListNode tail;
+    ListNode curr;
+    public void insert(int val)
+    {   
+        ListNode n = new ListNode(val);
+        if(head == null)
+        {
+            head = n;
+            tail = n;
+            curr = n;
+        }
+        else 
+        {
+            tail.next = n;
+            tail = n;
+        }
+    }
+    
+    public void display()
+    {
+        while(curr != null)
+        {
+            System.out.print(curr.val +" ");
+            curr = curr.next;
+        }
+        System.out.println();
+        curr = head;
+    }
+    public boolean isPalindrome(ListNode head) {
+        String s = "";
+        while(head != null)
+        {
+            s = s + head.val;
+            head = head.next;
+        }
+        return palindrome(s);
+    }
+    public Boolean palindrome(String s)
+    {
+        int left = 0;
+        int right = s.length()-1;
+        while(left < right)
+        {
+            if(s.charAt(left) != s.charAt(right))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+}
+class Main 
+{
+    public static void main(String[] args)
+    {
+        LinkedList n = new LinkedList();
+        n.insert(1);
+        n.insert(7);
+        n.insert(2);
+        n.insert(1);
+        n.display();
+        Boolean ans = n.isPalindrome(n.head);
+        System.out.print(ans);
+    }
+}
+*/
