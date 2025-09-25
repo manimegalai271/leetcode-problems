508f9b26d85d438b7ca68374b39608c6eaebf078
Manimegalai k <manimegalaik2710@gmail.com>
2025-09-25 15:05:22 +0530

    Time: 0 ms (100%), Space: 44.4 MB (31.64%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -28,3 +28,79 @@ class Solution {
 
     }
 }
+/*
+class ListNode 
+{
+    int val;
+    ListNode next;
+    ListNode(int val)
+    {
+        this.val = val;
+    }
+    
+}
+class LinkedList 
+{   
+    ListNode head;
+    ListNode tail;
+    ListNode curr;
+    public void insert(int val)
+    {   
+        ListNode n = new ListNode(val);
+        if(head == null)
+        {
+            head = n;
+            tail = n;
+            curr = n;
+        }
+        else 
+        {
+            tail.next = n;
+            tail = n;
+        }
+    }
+    
+    public void display()
+    {
+        while(curr != null)
+        {
+            System.out.print(curr.val +" ");
+            curr = curr.next;
+        }
+        System.out.println();
+        curr = head;
+    }
+    public ListNode deleteDuplicates(ListNode head)
+    {
+        ListNode curr = head;
+        if(curr == null)return null;
+        while(curr != null && curr.next != null)
+        {
+            if(curr.val == curr.next.val)
+            {
+                curr.next = curr.next.next;
+            }
+            else 
+            {
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}
+class Main 
+{
+    public static void main(String[] args)
+    {
+        LinkedList n = new LinkedList();
+        n.insert(5);
+        n.insert(5);
+        n.insert(7);
+        n.insert(10);
+        n.display();
+        n.deleteDuplicates(n.head);
+        n.display();
+        
+    }
+}
+*/
