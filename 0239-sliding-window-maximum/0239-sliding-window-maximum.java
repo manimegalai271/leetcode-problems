a16233cc35ac1a2b8fc88e0003bceed2fc41dfc4
Manimegalai k <manimegalaik2710@gmail.com>
2025-09-29 20:07:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-         int n = nums.length;
+        int n = nums.length;
         if (n == 0 || k == 0) return new int[0];
 
         int[] result = new int[n - k + 1];
-        Deque<Integer> dq = new ArrayDeque<>(); // stores indices
+            Queue<Integer> dq = new ArrayDeque<>(); // stores indices
 
         for (int i = 0; i < n; i++) {
             // Remove indices that are out of current window
