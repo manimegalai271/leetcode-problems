767dcba8fe211175d82373ec2f825b97cbc9e75f
Manimegalai k <manimegalaik2710@gmail.com>
2025-09-29 15:18:01 +0530

    Time: 2 ms (62.09%), Space: 42.6 MB (24.49%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int[] ans = new int[code.length];
+        int n = code.length;
+        if(k == 0)
+        {
+            return ans;
+        }
+        for(int i = 0; i < code.length;i++)
+        {   
+            int sum = 0;
+            if(k > 0)
+            {
+                for(int j = 1;j <= k;j++)
+                {
+                   sum += code[(i+j) % n];
+                }
+            }
+            else if(k < 0)
+            {
+                for(int j = 1;j <= -k;j++)
+                {
+                   sum += code[(i-j+n) % n];
+                }
+            }
+            ans[i] = sum;
+        }
+        return ans;
+    }
+}
