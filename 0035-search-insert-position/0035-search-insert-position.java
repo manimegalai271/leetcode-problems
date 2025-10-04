d9ae2314cf3eb8b91577edba9bc317ec931b963a
Manimegalai k <manimegalaik2710@gmail.com>
2025-10-04 11:34:11 +0530

    Time: 1 ms (2.34%), Space: 43.2 MB (29.03%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int[] num = new int[nums.length+1];
+        for(int i = 0;i < nums.length;i++)
+        {
+            num[i] = nums[i];
+        }
+        for(int i = 0;i < nums.length;i++)
+        {
+            if(nums[i] == target)
+            {
+                return i;
+            }
+        }
+        num[nums.length] = target;
+        Arrays.sort(num);
+        for(int i = 0;i < num.length;i++)
+        {
+            if(num[i] == target)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
+//Brute Force Apporach
