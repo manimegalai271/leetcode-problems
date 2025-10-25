83ac54d65400b6045d40792f2af87c28d84ee076
Manimegalai k <manimegalaik2710@gmail.com>
2025-10-25 18:07:08 +0530

    Time: 5 ms (96.64%), Space: 41.1 MB (98.85%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,46 +1,30 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n = s.length();
-        int count = n * (n+1) / 2;
-        String[] arr = new String[count];
-        int i = 0;
-        for(int k = 0;k < s.length();k++)
-        {   
-            String temp = "";
-            for(int j = k;j < s.length();j++)
-            {
-                temp = temp + s.charAt(j);
-                arr[i++] = temp;
-            }
-            temp = "";
-        }
-        int ans = 0;
-        for(String ss:arr)
-        {
-            if(palindrome(ss))
-            {
-                ans++;
-            }
-        }
-        return ans;
+        int count = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+
+      // Count palindromes with odd length
+      count += countPalindromes(s, i, i);
+
+      // Count palindromes with even length
+      count += countPalindromes(s, i, i + 1);
     }
-    public  Boolean palindrome(String ss)
-    {
-        int i = 0;
-        int j = ss.length()- 1;
-        while(i < j)
-        {
-            if(ss.charAt(i) != ss.charAt(j))
-            {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-        
+
+    return count;
     }
+    public int countPalindromes(String s, int left, int right) {
+
+    int count = 0;
+
+    // Check for palindromes
+    while (left >= 0 && right < s.length()
+        && s.charAt(left) == s.charAt(right)) {
+      count++;
+      left--;
+      right++;
+    }
+
+    return count;
+  }
 }
-//brute force apporach 
-//Time complexity = o(n ^ 3)
-//space complexity = o(n ^ 3);
