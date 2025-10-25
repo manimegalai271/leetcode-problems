8c6103f05a6cdcd5cca44655759a9d48e5fac2ab
Manimegalai k <manimegalaik2710@gmail.com>
2025-10-25 11:37:17 +0530

    Time: 1 ms (99.65%), Space: 43.8 MB (97.32%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,21 +1,19 @@
 class Solution {
     public int compress(char[] chars) {
-        int i = 0; // position to write compressed result
+        int i = 0;
+        int count = 1;
         
-        for (int j = 1, count = 1; j <= chars.length; j++, count++) {
-            // when current char is different from previous OR end of array
+        for (int j = 1; j <= chars.length; j++, count++) {
             if (j == chars.length || chars[j] != chars[j - 1]) {
-                chars[i++] = chars[j - 1]; // write the character
-                
-                // if count > 1, convert count to string and write each digit
-                if (count > 1) {
-                    for (char digit : String.valueOf(count).toCharArray()) {
-                        chars[i++] = digit;
+                chars[i++] = chars[j - 1];
+                if (count >= 2) {
+                    for (char c : Integer.toString(count).toCharArray()) {
+                        chars[i++] = c;
                     }
                 }
-                count = 0; // reset count for next group
+                count = 0;
             }
         }
-        return i; // new length of compressed array
+        return i;
     }
 }
