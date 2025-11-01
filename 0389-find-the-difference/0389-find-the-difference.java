17e6b80eef27543804c7aca7c0ea2f3f1a644ed3
Manimegalai k <manimegalaik2710@gmail.com>
2025-11-01 18:03:27 +0530

    Time: 8 ms (19.36%), Space: 43 MB (5.69%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,19 +1,18 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        StringBuilder tBuilder = new StringBuilder(t);
-        
-        for (int i = 0; i < s.length(); i++) {
+        StringBuilder st = new StringBuilder(t);
+        for(int i = 0;i < s.length();i++)
+        {
             char ch = s.charAt(i);
-            // find and remove the matching char in t
-            for (int j = 0; j < tBuilder.length(); j++) {
-                if (tBuilder.charAt(j) == ch) {
-                    tBuilder.deleteCharAt(j);
+            for(int j = 0;j < st.length();j++)
+            {
+                if(st.charAt(j) == ch)
+                {
+                    st.deleteCharAt(j);
                     break;
                 }
             }
         }
-        
-        // Remaining character is the extra one
-        return tBuilder.charAt(0);
+        return st.charAt(0);
     }
 }
