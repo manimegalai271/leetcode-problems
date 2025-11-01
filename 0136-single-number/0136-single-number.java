67588f23e1b2b69eaaeab0d72a848aab961419d5
Manimegalai k <manimegalaik2710@gmail.com>
2025-11-01 16:43:07 +0530

    Time: 15 ms (9.71%), Space: 46.8 MB (6.02%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,24 @@
 class Solution {
     public int singleNumber(int[] nums) {
-       int bit=nums[0];
-       for(int i=1;i<nums.length;i++)
-       {
-          
-            bit=bit^nums[i];
-           
-       }
-       return bit;
+        Map<Integer,Integer> freq = new HashMap<>();
+        for(int i = 0;i < nums.length;i++)
+        {
+            if(freq.containsKey(nums[i]))
+            {
+                freq.put(nums[i],freq.get(nums[i]) + 1);
+            }
+            else 
+            {
+                freq.put(nums[i],1);
+            }
+        }
+        for(int key:freq.keySet())
+        {
+            if(freq.get(key) == 1)
+            {
+                return key;
+            }
+        }
+        return 0;
     }
 }
