ec0976829870e9f48da0124e976ac4390dc4c4a6
Manimegalai k <manimegalaik2710@gmail.com>
2025-11-02 12:53:56 +0530

    Time: 6 ms (85.31%), Space: 57.5 MB (31.95%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,23 +1,16 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> ans = new ArrayList<>();
-        Map<Integer,Integer> freq = new HashMap<>();
         for(int i = 0;i < nums.length;i++)
         {
-            if(freq.containsKey(nums[i]))
+            int index = Math.abs(nums[i]) - 1;
+            if(nums[index] < 0)
             {
-                freq.put(nums[i],freq.get(nums[i]) + 1);
+                ans.add(Math.abs(nums[i]));
             }
             else 
             {
-                freq.put(nums[i],1);
-            }
-        }
-        for(int key:freq.keySet())
-        {
-            if(freq.get(key) == 2)
-            {
-                ans.add(key);
+                nums[index] = -nums[index];
             }
         }
         return ans;
