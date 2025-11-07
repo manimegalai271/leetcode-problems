1195506c4f908edc6754c0045173fc75f17553de
Manimegalai k <manimegalaik2710@gmail.com>
2025-11-07 22:27:12 +0530

    Time: 2 ms (47.54%), Space: 47.3 MB (6.25%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,31 +1,22 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
         int n = nums.length;
-        if (k <= 0 || k > n) {
-            throw new IllegalArgumentException("k must be between 1 and nums.length");
-        }
-
         int[] ans = new int[n - k + 1];
-
-        for (int i = 0; i <= n - k; i++) {            // start of window
-            int max = Integer.MIN_VALUE;
-            boolean flag = false;
-
-            // iterate the whole window from i to i+k-1
-            for (int j = i; j < i + k; j++) {
-                // check ascending with next element (only if next exists in window)
-                if (j < i + k - 1 && nums[j] >= nums[j + 1]) {
-                    flag = true;
+        for(int i = 0;i < n - k + 1;i++)
+        {
+            int max = nums[i];
+            Boolean isconsecutive = true;
+            for(int j = i;j < i + k - 1;j++)
+            {
+                if(nums[j]+1 != nums[j+1])
+                {
+                    isconsecutive = false;
                     break;
                 }
-                // update max for every element in the window
-                if (nums[j] > max) max = nums[j];
+                max =nums[j + 1];
             }
-
-            if (flag) ans[i] = -1;
-            else ans[i] = max;
+            ans[i] = isconsecutive?max:-1; 
         }
-
         return ans;
     }
 }
