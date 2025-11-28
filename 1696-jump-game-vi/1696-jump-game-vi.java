73036072d20d6e08b0e180eac7f810dffa790736
Manimegalai k <manimegalaik2710@gmail.com>
2025-11-28 14:01:11 +0530

    Time: 23 ms (88.18%), Space: 88.8 MB (13%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.offer(0);
+        for(int i = 1;i < nums.length;i++)
+        {
+            if(dq.peekFirst()+ k < i)
+            {
+                dq.pollFirst();
+            }
+            nums[i] += nums[dq.peekFirst()];
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i])
+            {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        }
+        return nums[nums.length - 1];
+    }
+}
