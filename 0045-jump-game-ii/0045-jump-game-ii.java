f7a922b0c2671a48b3453dc51569596f5a58837d
Manimegalai k <manimegalaik2710@gmail.com>
2025-12-02 11:41:36 +0530

    Time: 1 ms (99.27%), Space: 47.1 MB (32.05%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,31 +1,23 @@
 class Solution {
     public int jump(int[] nums) {
-        int totalJumps = 0;
-
-    // destination is last index
-    int destination = nums.length - 1;
-
-    int coverage = 0, lastJumpIdx = 0;
-
-    // Base case
-    if (nums.length == 1) return 0;
-
-    // Greedy strategy: extend coverage as long as possible
-    for (int i = 0; i < nums.length; i++) {
-
-      coverage = Math.max(coverage, i + nums[i]);
-
-      if (i == lastJumpIdx) {
-        lastJumpIdx = coverage;
-        totalJumps++;
-
-        // check if we reached destination already
-        if (coverage >= destination) {
-          return totalJumps;
+        if(nums.length == 1)return 0;
+        int minjumpcount = 0;
+        int destination = nums.length - 1;
+        int coverage = 0;
+        int lastjump = 0;
+        for(int i = 0;i < nums.length;i++)
+        {
+            coverage = Math.max(coverage,i + nums[i]);
+            if(i == lastjump)
+            {
+                minjumpcount++;
+                lastjump = coverage;
+                if(coverage >= destination)
+                {
+                    return minjumpcount;
+                }
+            }
         }
-      }
-    }
-
-    return totalJumps;
+        return 0;
     }
 }
