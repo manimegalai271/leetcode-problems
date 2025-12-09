625b74077332ca33b6640b1b8d73d416974db112
Manimegalai k <manimegalaik2710@gmail.com>
2025-12-10 00:01:02 +0530

    Time: 4 ms (92.04%), Space: 47.1 MB (28.52%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean dfs(List<Integer> res,List<List<Integer>> graph,int[] colors,int i)
+    {
+        colors[i] = 1;
+        for(int neighbor:graph.get(i))
+        {
+            if(colors[neighbor] == 1)return true;
+            if(colors[neighbor] == 0)
+            {
+                if(dfs(res,graph,colors,neighbor))return true;
+            }
+        }
+        colors[i] = 2;
+        res.add(i);
+        return false;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] colors = new int[numCourses];
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i = 0;i < numCourses;i++)
+        {
+            graph.add(new ArrayList<>());
+        }
+        for(int pre[]:prerequisites)
+        {
+            graph.get(pre[0]).add(pre[1]);
+        }
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0;i < numCourses;i++)
+        {
+            if(colors[i] == 0)
+            {
+                if(dfs(res,graph,colors,i))return false;
+            }
+        }
+        return true;
+    }
+}
