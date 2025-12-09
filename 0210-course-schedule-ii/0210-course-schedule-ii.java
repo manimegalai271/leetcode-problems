260184b561fe1479922f2329afe717d2f27801db
Manimegalai k <manimegalaik2710@gmail.com>
2025-12-09 23:58:32 +0530

    Time: 3 ms (99.35%), Space: 46.9 MB (34.23%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,44 +1,43 @@
 class Solution {
-    public boolean dfs(List<List<Integer>> graph, int[] colors, List<Integer> res, int i) {
-        colors[i] = 1; // visiting
-        
-        for (int neighbor : graph.get(i)) {
-            if (colors[neighbor] == 1) return true; // cycle found
-            if (colors[neighbor] == 0) {
-                if (dfs(graph, colors, res, neighbor)) return true;    
+    public boolean dfs(List<Integer> res,List<List<Integer>> graph,int[] colors,int i)
+    {
+        colors[i] = 1;
+        for(int neighbor:graph.get(i))
+        {
+            if(colors[neighbor] == 1)return true;
+            if(colors[neighbor] == 0)
+            {
+                if(dfs(res,graph,colors,neighbor))return true;
             }
         }
-        
-        colors[i] = 2;  
+        colors[i] = 2;
         res.add(i);
         return false;
     }
-    
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] colors = new int[numCourses];
         List<List<Integer>> graph = new ArrayList<>();
-        
-        for (int i = 0; i < numCourses; i++) {
+        for(int i = 0;i < numCourses;i++)
+        {
             graph.add(new ArrayList<>());
         }
-        
-        // build graph
-        for (int[] pre : prerequisites) {
+        for(int pre[]:prerequisites)
+        {
             graph.get(pre[0]).add(pre[1]);
         }
-        
         List<Integer> res = new ArrayList<>();
-        
-        for (int i = 0; i < numCourses; i++) {
-            if (colors[i] == 0) {
-                if (dfs(graph, colors, res, i)) return new int[0];
+        for(int i = 0;i < numCourses;i++)
+        {
+            if(colors[i] == 0)
+            {
+                if(dfs(res,graph,colors,i))return new int[0];
             }
         }
-        
-        // convert List to array
         int[] ans = new int[res.size()];
-        for (int i = 0; i < res.size(); i++) ans[i] = res.get(i);
-        
+        for(int i = 0;i < res.size();i++)
+        {
+            ans[i]= res.get(i);
+        }
         return ans;
     }
 }
