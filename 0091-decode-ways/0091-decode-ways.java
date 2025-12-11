3b907e24db85837b1eed51a3dca08c3578087508
Manimegalai k <manimegalaik2710@gmail.com>
2025-12-11 12:42:33 +0530

    Time: 1 ms (85.23%), Space: 42.7 MB (63.83%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,28 +1,26 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        if (n == 0) return 0;
-
+        if(n == 0)return 0;
+        if(s.charAt(0) == '0')return 0;
         int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
-
-        for (int i = 2; i <= n; i++) {
-
-            // One digit decode (last char)
-            int oneDigit = s.charAt(i - 1) - '0';
-            if (oneDigit >= 1)
+        if(s.charAt(0) != '0')dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ?0:1;
+        for(int i = 2;i <= n;i++)
+        {
+            int onedigit = s.charAt(i - 1) - '0';
+            if(onedigit >= 1)
+            {
                 dp[i] += dp[i - 1];
-
-            // Two digit decode (last two chars)
-            int tens = s.charAt(i - 2) - '0';
+            }
             int ones = s.charAt(i - 1) - '0';
-            int twoDigits = tens * 10 + ones;
-
-            if (twoDigits >= 10 && twoDigits <= 26)
-                dp[i] += dp[i - 2];
+            int twos  = s.charAt(i - 2) - '0';
+            int twodigit = twos * 10 + ones;
+            if(twodigit >= 10 && twodigit <= 26)
+            {
+                dp[i] += dp[i-2];
+            }
         }
-
         return dp[n];
     }
 }
