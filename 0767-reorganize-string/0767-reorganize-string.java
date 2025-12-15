a969d5fbeeb8dc162218efa8cf6753958cd59f2d
Manimegalai k <manimegalaik2710@gmail.com>
2025-12-15 21:41:24 +0530

    Time: 1 ms (95.39%), Space: 43 MB (36.52%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,42 +1,43 @@
 class Solution {
     public String reorganizeString(String s) {
-        Map<Character,Integer> freqMap = new HashMap<>();
-        for(int i = 0;i < s.length();i++)
-        {   
-            char ch = s.charAt(i);
-            if(!freqMap.containsKey(ch))
-            {
-                freqMap.put(ch,0);
+
+        int[] hash = new int[26];
+
+        // Count frequency
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i) - 'a']++;
+        }
+
+        // Find the character with maximum frequency
+        int max = 0, letter = 0;
+        for (int i = 0; i < 26; i++) {
+            if (hash[i] > max) {
+                max = hash[i];
+                letter = i;
             }
-            freqMap.put(ch,freqMap.get(ch) + 1);
-        }
-        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a,b)->(freqMap.get(b) - freqMap.get(a)));
-        for(char c:freqMap.keySet())
-        {
-            maxHeap.add(c);
-        }
-        StringBuilder res = new StringBuilder();
-         while (maxHeap.size() >= 2) {
-            char char1 = maxHeap.poll();
-            char char2 = maxHeap.poll();
-
-            res.append(char1);
-            res.append(char2);
-
-            freqMap.put(char1, freqMap.get(char1) - 1);
-            freqMap.put(char2, freqMap.get(char2) - 1);
-
-            if (freqMap.get(char1) > 0) maxHeap.add(char1);
-            if (freqMap.get(char2) > 0) maxHeap.add(char2);
         }
 
-        if (!maxHeap.isEmpty()) {
-            char ch = maxHeap.poll();
-            if (freqMap.get(ch) > 1) return "";
-            res.append(ch);
+        // If not possible to reorganize
+        if (max > (s.length() + 1) / 2) return "";
+
+        char[] res = new char[s.length()];
+
+        // Fill even indices with the most frequent character
+        int idx = 0;
+        while (hash[letter]-- > 0) {
+            res[idx] = (char) (letter + 'a');
+            idx += 2;
         }
 
-        return res.toString();
+        // Fill remaining characters
+        for (int i = 0; i < 26; i++) {
+            while (hash[i]-- > 0) {
+                if (idx >= res.length) idx = 1;
+                res[idx] = (char) (i + 'a');
+                idx += 2;
+            }
+        }
+
+        return String.valueOf(res);
     }
 }
-//o(nlogn) o(n)
