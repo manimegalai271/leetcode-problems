f9f89c040aae3d96597991d2cfbc2740d660872a
Manimegalai k <manimegalaik2710@gmail.com>
2025-12-15 22:10:33 +0530

    Time: 1 ms (95.39%), Space: 43 MB (48.09%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,14 +1,9 @@
 class Solution {
     public String reorganizeString(String s) {
-
         int[] hash = new int[26];
-
-        // Count frequency
         for (int i = 0; i < s.length(); i++) {
             hash[s.charAt(i) - 'a']++;
         }
-
-        // Find the character with maximum frequency
         int max = 0, letter = 0;
         for (int i = 0; i < 26; i++) {
             if (hash[i] > max) {
@@ -16,20 +11,13 @@ class Solution {
                 letter = i;
             }
         }
-
-        // If not possible to reorganize
         if (max > (s.length() + 1) / 2) return "";
-
         char[] res = new char[s.length()];
-
-        // Fill even indices with the most frequent character
         int idx = 0;
         while (hash[letter]-- > 0) {
             res[idx] = (char) (letter + 'a');
             idx += 2;
         }
-
-        // Fill remaining characters
         for (int i = 0; i < 26; i++) {
             while (hash[i]-- > 0) {
                 if (idx >= res.length) idx = 1;
@@ -37,7 +25,6 @@ class Solution {
                 idx += 2;
             }
         }
-
         return String.valueOf(res);
     }
 }
