9ad7dd45161dfa396e474b1221a82ea23acda00a
Manimegalai k <manimegalaik2710@gmail.com>
2026-01-29 21:03:26 +0530

    Time: 1 ms (82.06%), Space: 42.7 MB (92.6%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -1,17 +1,20 @@
 class Solution {
     public int minimumMoves(String s) {
-        int moves = 0;
         int i = 0;
+        int count = 0;
 
-        while (i < s.length()) {
-            if (s.charAt(i) == 'X') {
-                moves++;
-                i += 3;   // convert this and next two characters
-            } else {
+        while(i < s.length())
+        {
+            if(s.charAt(i) == 'X')
+            {
+                count++;
+                i += 3;
+            }
+            else 
+            {
                 i++;
             }
         }
-
-        return moves;
+        return count;
     }
 }
