1ee61d8ee61903572eb2a337d217047526bd81f6
Manimegalai k <manimegalaik2710@gmail.com>
2026-02-20 11:57:45 +0530

    Time: 22 ms (7.07%), Space: 43 MB (7.02%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,25 +1,12 @@
 class Solution {
     public int mySqrt(int x) {
-        long left=1;
-        long right=x;
-        int ans=0;
-        while(left<=right)
+        for(long i = 1;i <= x;i++)
         {
-            long mid=(left+right)/2;
-            if(mid*mid==x)
+            if(i * i > x)
             {
-                return (int)mid;
-            }
-            else if(mid*mid>=x)
-            {
-                right=mid-1;
-            }
-            else if(mid*mid<=x)
-            {
-                left=mid+1;
-                ans=(int)mid;
+                return (int)i - 1;
             }
         }
-        return (int)ans;
+        return x;
     }
 }
