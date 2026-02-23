7b4a1cee16bae2c27a3248101011c7fd916a0bae
Manimegalai k <manimegalaik2710@gmail.com>
2026-02-23 09:57:17 +0530

    Time: 0 ms (100%), Space: 42 MB (75.83%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,19 +1,23 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if (num < 2) return true; // 0 and 1 are perfect squares
+        if(num < 2)return true;
 
-        long left = 2, right = num / 2;
-
-        while (left <= right) {
+        long left = 2;
+        long right = num / 2;;
+        while(left <= right)
+        {
             long mid = left + (right - left) / 2;
-            long sq = mid * mid;
-
-            if (sq == num) {
+            if(mid * mid == num)
+            {
                 return true;
-            } else if (sq < num) {
-                left = mid + 1;
-            } else {
+            }
+            else if(mid * mid > num)
+            {
                 right = mid - 1;
+            }
+            else if(mid * mid < num)
+            {
+                left = mid + 1;
             }
         }
         return false;
