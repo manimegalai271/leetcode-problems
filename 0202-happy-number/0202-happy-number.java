f6c0a52068fc289f7af49dee37c50c737aa2ca08
Manimegalai k <manimegalaik2710@gmail.com>
2026-02-25 09:39:58 +0530

    Time: 0 ms (100%), Space: 41.2 MB (99.98%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,13 +1,23 @@
 class Solution {
+    public int compute(int n)
+       {
+        int res =0;
+        while(n != 0)
+        {
+            res = res+((n%10) * (n % 10));
+            n /=10;
+        }
+        return res;
+       }
     public boolean isHappy(int n) {
-       if(n==1 || n==7)
-       {
-        return true;
-       }
-       if(n>1 && n<10)
-       {
-        return false;
-       }
-       return n%9!=0;
+       int slow = n,fast =n;
+       do{
+        slow = compute(slow);
+        fast =compute(fast);
+        fast =compute(fast);
+       }while(slow != fast);
+       if(slow == 1) return true;
+       return false;
+
     }
 }
