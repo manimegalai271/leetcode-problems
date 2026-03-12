925e24b3a0d62419f5fcfc2afc64bf780da79aa2
Manimegalai k <manimegalaik2710@gmail.com>
2026-03-12 11:45:01 +0530

    Time: 474 ms (49.5%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,2 @@
-SELECT today.id AS Id
-FROM Weather yesterday 
-CROSS JOIN Weather today 
-WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1 AND
-   yesterday.temperature < today.temperature;
-
+SELECT today.id as id 
+FROM Weather yesterday CROSS JOIN Weather today WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1 && today.temperature > yesterday.temperature;
