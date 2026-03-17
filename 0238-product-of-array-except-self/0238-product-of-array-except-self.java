0f2f6060f788d62930e1ab84391947e22c46fefe
Manimegalai k <manimegalaik2710@gmail.com>
2026-03-17 14:37:07 +0530

    Time: 2 ms (94.62%), Space: 72 MB (58.23%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,21 +1,33 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int[] lp = new int[nums.length];
-        int[] rp = new int[nums.length];
-        lp[0] = 1;
-        rp[nums.length -1] = 1;
-        for(int i = 0;i < nums.length -1;i++)
+    public int[] productExceptSelf(int[] num) {
+        int[] ans = new int[num.length];
+        int total_product = 1;
+        int zero_count = 0;
+        int mul = 1;
+        for(int i = 0;i < num.length;i++)
         {
-            lp[i + 1] = lp[i] * nums[i];
+            if(num[i] == 0)
+            {
+                zero_count++;
+            }
+            else if(num[i] != 0)
+            {
+                mul *= num[i];
+            }
+            total_product *= num[i];
         }
-        for(int i = nums.length-1;i > 0;i--)
-        {
-            rp[i-1] = rp[i] * nums[i];
+        for(int i = 0;i < ans.length;i++)
+        {   
+            if(zero_count == 1 && num[i] == 0)
+            {
+                ans[i] = mul;
+            }
+            if(total_product != 0 && zero_count != 1)
+            {
+                ans[i] = total_product / num[i];
+            }
+            
         }
-        for(int i = 0;i < nums.length;i++)
-        {
-            nums[i] = lp[i] * rp[i];
-        }
-        return nums;
+        return ans;
     }
 }
