3f61549b442ad15e5f427599e6d619e1016c00b1
Manimegalai k <manimegalaik2710@gmail.com>
2026-03-18 16:03:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,40 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int zero_count = 0;
+        int ones_count = 0;
+        int twos_count = 0;
+        for(int i = 0;i < nums.length;i++)
+        {
+            if(nums[i] == 0)
+            {
+                zero_count++;
+            }
+            else if(nums[i] == 1)
+            {
+                ones_count++;
+            }
+            else if(nums[i] == 2)
+            {
+                twos_count++;
+            }
+        }
+        for(int i = 0;i < nums.length;i++)
+        {
+            if(zero_count > 0)
+            {
+                nums[i] = 0;
+                zero_count--;
+            }
+            else if(ones_count > 0)
+            {
+                nums[i] = 1;
+                ones_count--;
+            }
+            else if(twos_count > 0)
+            {
+                nums[i] = 2;
+                twos_count--;
+            }
+        }
     }
 }
