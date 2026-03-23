36ba19ec08f5c5feed47619f3595b152cdbb97ae
Manimegalai k <manimegalaik2710@gmail.com>
2026-03-23 14:06:46 +0530

    Time: 3 ms (100%), Space: 47.8 MB (18.71%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,48 +1,20 @@
 class Solution {
     public int minimumLength(String s) {
-        int len = s.length();
-        if(s.charAt(0) != s.charAt(len - 1))
-        {
-            return len;
-        }
-        char[] ch = s.toCharArray();
         int left = 0;
-        int right = ch.length - 1;
-        while(right < ch.length && left < right)
-        {
-            if(ch[left] != ch[right])
-            {
-                break;
-            }
-            int ls = left;
-            while(left < right && left < ch.length  && ch[left] == ch[left + 1])
+        int right = s.length() - 1;
+        while(left < right && s.charAt(left) == s.charAt(right))
+        {   
+            char ch = s.charAt(left);
+            while(left <= right && s.charAt(left) == ch)
             {
                 left++;
             }
-            int rs = right;
-            while(left < right && right < ch.length && ch[right] == ch[right - 1])
+            while(left <= right && s.charAt(right) == ch)
             {
                 right--;
             }
-            for(int i = ls;i <= left;i++)
-            {
-                ch[i] = '@';
-            }
-            for(int i = rs;i >= right;i--)
-            {
-                ch[i] = '@';
-            }
-            left++;
-            right--;
         }
-        int ans = 0;
-        for(int i = 0;i < ch.length;i++)
-        {
-            if(ch[i] != '@')
-            {
-                ans++;
-            }
-        }
-        return ans;
+        return right - left + 1;
+
     }
 }
