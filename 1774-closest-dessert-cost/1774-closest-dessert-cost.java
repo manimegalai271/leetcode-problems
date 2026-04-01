6377dc3282888244a65c5b7d81d6c36178518489
Manimegalai k <manimegalaik2710@gmail.com>
2026-04-01 11:05:31 +0530

    Time: 18 ms (31.48%), Space: 42.7 MB (79.01%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.java
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.java
@@ -1,25 +1,27 @@
 class Solution {
+    public int costt(int cost,int closest,int i,int[] toppingCosts,int target)
+    {
+        if (Math.abs(cost - target) < Math.abs(closest - target) ||
+           (Math.abs(cost - target) == Math.abs(closest - target) && cost < closest)) {
+            closest = cost;
+        }
+         if(i == toppingCosts.length)
+         {
+             return closest;
+         }
+
+         closest = costt(cost,closest,i+1,toppingCosts,target);
+         closest = costt(cost+toppingCosts[i],closest,i+1,toppingCosts,target);
+         closest = costt(cost+(toppingCosts[i] * 2),closest,i+1,toppingCosts,target);
+
+         return closest;
+    }
     public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
         int closest = Integer.MAX_VALUE;
         for(int base:baseCosts)
         {
-            closest =  close(closest,base,0,toppingCosts,target);
+            closest = costt(base,closest,0,toppingCosts,target);
         }
         return closest;
-    }
-    public int close(int closes,int cost,int i,int[] toppingCosts,int target)
-    {
-        if(Math.abs(cost - target) < Math.abs(closes - target) ||(Math.abs(cost - target) == Math.abs(closes - target) && cost < closes))
-        {
-            closes = cost;
-        }
-        if(i == toppingCosts.length)return closes;
-
-        closes = close(closes,cost,i+1,toppingCosts,target);
-        closes = close(closes,cost+toppingCosts[i],i+1,toppingCosts,target);
-        closes = close(closes,cost+toppingCosts[i] * 2,i+1,toppingCosts,target);
-
-        return closes;
-        
     }
 }
