01c1360850e33894588d6f67c8446c00e63bf57a
Manimegalai k <manimegalaik2710@gmail.com>
2026-04-15 21:42:26 +0530

    Time: 2 ms (32.96%), Space: 47.4 MB (24.92%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,23 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
-        if(nums.length == 1)return 0;
-        int minjumpcount = 0;
-        int destination = nums.length - 1;
-        int coverage = 0;
-        int lastjump = 0;
-        for(int i = 0;i < nums.length;i++)
-        {
-            coverage = Math.max(coverage,i + nums[i]);
-            if(i == lastjump)
-            {
-                minjumpcount++;
-                lastjump = coverage;
-                if(coverage >= destination)
-                {
-                    return minjumpcount;
-                }
+        int jumps = 0;
+        int currEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i == currEnd) {
+                jumps++;
+                currEnd = farthest;
             }
         }
-        return 0;
+
+        return jumps;
     }
 }
