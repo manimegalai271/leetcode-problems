50f1917d4c20262b334f21c0b6a64c2e78a76fa7
Manimegalai k <manimegalaik2710@gmail.com>
2026-06-05 14:31:59 +0530

    Time: 21 ms (34.1%), Space: 43.8 MB (52.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,28 +1,35 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
-        
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            // Odd length palindrome
-            int len1 = expandAroundCenter(s, i, i);
-            // Even length palindrome
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+        int maxlen = 1;
+        int start = 0;
+        for(int i = 1;i < s.length();i++)
+        {
+            int l = i - 1;
+            int r = i;
+            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r))
+            {
+                if(maxlen < (r - l + 1)){
+                    maxlen = r - l + 1;
+                    start = l;
+                }
+                l -= 1;
+                r += 1;
+            }
+            l = i - 1;
+            r = i + 1;
+            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r))
+            {
+                
+                if(maxlen < (r - l + 1)){
+                    maxlen = r - l + 1;
+                    start = l;
+                }
+                l -= 1;
+                r += 1;
             }
         }
-        return s.substring(start, end + 1);
-    }
-
-    private int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return right - left - 1; // length of palindrome
+        System.out.print(maxlen);
+        System.out.print(start);
+        return s.substring(start,start + maxlen );
     }
 }
